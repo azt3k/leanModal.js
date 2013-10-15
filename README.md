45b58482890224bd6cbf4c1c4d8f58b7d34af150
azt3k <breaks.nz@gmail.com>
2013-10-15 21:02:07 +1300

    added callbacks and updated reademe

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 A super simple JQuery plugin for modal windows. works with your CSS.
 
-Built for all the short dialogs, alerts, panels and such associated with an app, that you may want to handle in a modal window. Designed to handle hidden content, and doesn't apply any styles to the target element, other than for displaying and positioning.
+Built for all the short dialogs, alerts, panels and such associated with an app,
+that you may want to handle in a modal window. Designed to handle hidden
+content, and doesn’t apply any styles to the target element, other than for
+displaying and positioning.
+
+–
+
+Usage exactly like the old one except you can invoke it like this
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+$('body').append(   
+    '<div class="modal" id="video-iframe" style="display:none;">' + 
+        '<iframe id="ytplayer" ' +
+            'type="text/html" ' + 
+            'src="' + url + '" ' + 
+            'frameborder="0" ' + 
+            'allowfullscreen> ' + 
+        '</iframe>' +
+    '</div>'
+);
+
+$('<a data-modal="#video-iframe"></a>') .leanModal({ top : '50%', overlay : 0.4, closeButton: ".close", attr: 'data-modal' })
+                                        .trigger('click'); 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+and has more options
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{  
+              top:'100px',  
+          overlay: 0.5,  
+      closeButton: null,  
+             attr: 'href',  
+        eventType: 'click',  
+    onBeforeClose: null,  
+     onAfterClose: null,  
+     onBeforeShow: null,  
+      onAfterShow: null
+}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -9,20 +9,29 @@
 
             this.init = function(options) {
 
+                // bind this to a local var with a diff name
                 var _this = this;                
 
+                // configure
                 this.options = $.extend({
                     top: '100px',
                     overlay: 0.5,
                     closeButton: null,
-                    attr: 'href'
+                    attr: 'href',
+                    eventType: 'click',
+                    onBeforeClose: null,
+                    onAfterClose: null,
+                    onBeforeShow: null,
+                    onAfterShow: null                    
                 }, options);
                 
+                // remove the overlay if present and add a new one
                 $('#lean_overlay').remove();
                 $("body").append("<div id='lean_overlay'></div>"); 
 
+                // Bind the modal to show to a click
                 this.each(function() {
-                    $(this).click(function(e) {
+                    $(this).on(_this.options.eventType, function(e) {
                         _this.show_modal($($(this).attr(_this.options.attr)));
                         e.preventDefault();
                     });
@@ -31,6 +40,10 @@
             };            
 
             this.show_modal = function($selector) {
+
+                // call back
+                var b_func = this.options.onBeforeShow;
+                if (typeof b_func == 'function') b_func();
 
                 // vars
                 var _this = this,
@@ -58,11 +71,26 @@
                             })
                             .fadeTo(200,1);
 
+                // call back
+                var a_func = this.options.onAfterShow;
+                if (typeof a_func == 'function') a_func();                            
+
             };
 
             this.close_modal = function($selector) {
+
+                // call back
+                var b_func = this.options.onBeforeClose;
+                if (typeof b_func == 'function') b_func();
+
+                // close it
                 $("#lean_overlay").fadeOut(200);
                 $selector.css({ 'display' : 'none' });
+
+                // call back
+                var b_func = this.options.onBeforeClose
+                if (typeof b_func == 'function') b_func();;
+
             };
 
             // Init
